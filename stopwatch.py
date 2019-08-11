a9357b9495e9897e8b6ca7068c9236d86bf6d652
hupmake <branden@hupman.org>
2019-08-11 19:29:45 -0400

    Added Ctrl-C catch and removed unnecessary import.

--- stopwatch.py
+++ stopwatch.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python
-# Python program that runs a stopwatch. Start, stop, reset, etc.
-# So far, it will count seconds, minutes, and hours. It just goes. Stops on CTRL+C.
+# Python program that runs a stopwatch. Start and stop. No pause.
+# So far, it will count seconds, minutes, and hours. It just goes.
+# Stops on Ctrl-C and outputs the recorded time.
 
 # version 0.2.1
 
 # Imports!
 # This is running on Linux, so we'll just do linuxy imports.
 import time
-import sys
+#import sys
 
 # Building blocks!
 
 hour = 0
 mins = 0
 secs = 0
-#timerContinue = 1
 
 def run_time():
 	global hour
 	global mins
 	global secs
-	time_string = str("{0:0=2d}".format(hour)) + str("h ") + str("{0:0=2d}".format(mins)) + str("m ") + str("{0:0=2d}".format(secs)) + str("s")
+	run_time.time_string = str("{0:0=2d}".format(hour)) + str("h ") + str("{0:0=2d}".format(mins)) + str("m ") + str("{0:0=2d}".format(secs)) + str("s")
 
-	print(time_string, end = '')
+	print(run_time.time_string, end = '')
 	print('\r', end = '', flush = 'True')
 
 	if hour == 23 and mins == 59 and secs == 59:
 		hour = 0
 		mins = 0
 		secs = 0
-#		timerContinue = 0
 		print("1 day reached. Timer ended.")
 		time.sleep(1)
 
@@ -49,4 +48,9 @@ def run_time():
 		time.sleep(1)
 
 while True:
-	run_time()
+	try:
+		run_time()
+		pass
+	except KeyboardInterrupt as e:
+		print('\n Clocked',run_time.time_string)
+		break
