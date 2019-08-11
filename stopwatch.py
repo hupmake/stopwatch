1f69a633c20cb41b62510de95d3c7ca1a8264956
hupmake <branden@hupman.org>
2019-08-11 19:46:42 -0400

    Version 1.0.0 complete!

--- stopwatch.py
+++ stopwatch.py
@@ -6,22 +6,21 @@
 # version 0.2.1
 
 # Imports!
-# This is running on Linux, so we'll just do linuxy imports.
 import time
-#import sys
 
 # Building blocks!
-
 hour = 0
 mins = 0
 secs = 0
 
+# The main (only?) function.
 def run_time():
 	global hour
 	global mins
 	global secs
+# Set the time counter string an make it accessible outside of the func.
 	run_time.time_string = str("{0:0=2d}".format(hour)) + str("h ") + str("{0:0=2d}".format(mins)) + str("m ") + str("{0:0=2d}".format(secs)) + str("s")
-
+# Overwrite the used line when the script is running in real time.
 	print(run_time.time_string, end = '')
 	print('\r', end = '', flush = 'True')
 
@@ -30,27 +29,29 @@ def run_time():
 		mins = 0
 		secs = 0
 		print("1 day reached. Timer ended.")
+		raise KeyboardInterrupt
 		time.sleep(1)
 
 	elif mins == 59 and secs == 59:
 		secs = 0
 		mins = 0
 		hour +=1
-		time.sleep(1)
+		time.sleep(.001)
 
 	elif secs == 59:
 		secs = 0
 		mins += 1
-		time.sleep(1)
+		time.sleep(.001)
 
 	elif secs < 59:
 		secs += 1
-		time.sleep(1)
+		time.sleep(.001)
 
 while True:
 	try:
 		run_time()
 		pass
+# This catches the Ctrl-C
 	except KeyboardInterrupt as e:
 		print('\n Clocked',run_time.time_string)
 		break
